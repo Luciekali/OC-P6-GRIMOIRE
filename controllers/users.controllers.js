312d0c4eff61b08d6a43908bdd66317141ece2d2
Lucie Kalinowski <lucie.kalinowski@gmail.com>
2025-06-11 14:40:36 +0200

    login

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt')
 const { default: code } = require('http-status');
+const webToken = require('jsonwebtoken');
 
 exports.signUp = (req, res) => {
     const ROUNDS = 10
@@ -16,3 +17,30 @@ exports.signUp = (req, res) => {
         })
         .catch(error => res.status(code.INTERNAL_SERVER_ERROR).json({ error }));
 };
+
+exports.login = (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user === null) {
+                res.status(code.UNAUTHORIZED).json({ message: 'identifiant/mot de passe incorrect' });
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if (!valid) {
+                            res.status(code.UNAUTHORIZED).json({ message: 'identifiant/mot de passe incorrect' });
+                        } else {
+                            res.status(code.OK).json({
+                                userId: user._id,
+                                token: webToken.sign(
+                                    { userId: user._id },
+                                    process.env.TOKEN_KEY,
+                                    { expiresIn: '6h' }
+                                )
+                            });
+                        }
+                    })
+                    .catch(error => res.status(code.INTERNAL_SERVER_ERROR).json({ error }));
+            }
+        })
+        .catch(error => res.status(code.INTERNAL_SERVER_ERROR).json({ error }));
+}
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersCtrl = require('../controllers/users.controllers');
 
 router.post('/signup', usersCtrl.signUp);
+router.post('/login', usersCtrl.login)
 
 module.exports = router;

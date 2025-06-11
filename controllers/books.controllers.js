cb0b22dd9f18d856a425e305aa7c31bdb3fbb16c
Lucie Kalinowski <lucie.kalinowski@gmail.com>
2025-06-11 13:29:08 +0200

    lib status

--- controllers/books.controllers.js
+++ controllers/books.controllers.js
@@ -1,9 +1,10 @@
 const Book = require('../models/book');
+const { default: code } = require('http-status');
 
 exports.getAllBooks = (req, res) => {
     Book.find()
-        .then(books => res.status(200).json(books))
-        .catch(error => res.status(400).json({ error }))
+        .then(books => res.status(code.OK).json(books))
+        .catch(error => res.status(code.BAD_REQUEST).json({ error }))
 };
 
 exports.createBook = (req, res, next) => {
@@ -11,13 +12,13 @@ exports.createBook = (req, res, next) => {
         ...req.body
     });                  // creation book avec données du body
     book.save()
-        .then(() => res.status(201).json({ message: 'Livre créé !' }))
-        .catch(error => res.status(400).json({ error }));
+        .then(() => res.status(code.CREATED).json({ message: 'Livre créé !' }))
+        .catch(error => res.status(code.BAD_REQUEST).json({ error }));
 }
 
 exports.getOneBook = (req, res) => {
     Book.findOne({ _id: req.params.id })
-        .then(book => res.status(200).json(book))
-        .catch(error => res.status(400).json({ error }));
+        .then(book => res.status(code.OK).json(book))
+        .catch(error => res.status(code.BAD_REQUEST).json({ error }));
 }
 
--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,16 +1,18 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt')
+const { default: code } = require('http-status');
 
 exports.signUp = (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+    const ROUNDS = 10
+    bcrypt.hash(req.body.password, ROUNDS)
         .then(hash => {
             const user = new User({
                 email: req.body.email,
                 password: hash
             });
             user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-                .catch(error => res.status(400).json({ error }));
+                .then(() => res.status(code.OK).json({ message: 'Utilisateur créé !' }))
+                .catch(error => res.status(code.BAD_REQUEST).json({ error }));
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(code.INTERNAL_SERVER_ERROR).json({ error }));
 };
